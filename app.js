585aa6b6172f4d754d662cb1daa8ed1fa2ef383b
Agnes Ching <agnesching19@gmail.com>
2018-03-04 14:50:09 +0000

    using 3rd party modules

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-let res = notes.add(3, -5);
-console.log(res);
+// console.log(_.isString(true));
+// console.log(_.isString('Agnes'));
+
+let filteredArray = _.uniq(['Agnes', 'Agnes', 'Simon', 1, 3, 9, 1]);
+console.log(filteredArray);
+
+// let res = notes.add(3, -5);
+// console.log(res);
 
 // let user = os.userInfo();
 
