5929519e4ca24e73c005d9104b49ebd59ae9bf71
Agnes Ching <agnesching19@gmail.com>
2018-03-04 14:58:51 +0000

    nodemon

--- app.js
+++ app.js
@@ -1,28 +1,8 @@
 console.log('Starting app.js');
 
+// 3rd party modules
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+// From my own files
 const notes = require('./notes.js');
-
-// console.log(_.isString(true));
-// console.log(_.isString('Agnes'));
-
-let filteredArray = _.uniq(['Agnes', 'Agnes', 'Simon', 1, 3, 9, 1]);
-console.log(filteredArray);
-
-// let res = notes.add(3, -5);
-// console.log(res);
-
-// let user = os.userInfo();
-
-// // Option one
-// fs.appendFile('greeting.txt', `Hello ${user.username}! You are ${notes.age}.`, (err) => {
-//   if (err) {
-//     console.log('Unable to write to file');
-//   }
-// });
-
-// Option two
-// fs.appendFileSync('greeting.txt', 'Bello World!');
 
