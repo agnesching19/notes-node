102e4651103ecc7ff10252b4b505191dad9c81fe
Agnes Ching <agnesching19@gmail.com>
2018-03-04 14:07:52 +0000

    using require

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log('Starting app.');
+
+const fs = require('fs');
+const os = require('os');
+
+let user = os.userInfo();
+
+// Option one
+fs.appendFile('greeting.txt', `Hello ${user.username}!`, function(err) {
+  if (err) {
+    console.log('Unable to write to file');
+  }
+});
+
+// Option two
+// fs.appendFileSync('greeting.txt', 'Bello World!');
+
